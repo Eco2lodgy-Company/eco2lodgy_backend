5f10f3a796978c3d871a69cf1dace742ee0050d3
kounou25 <kounougilbert288@gmail.com>
2025-04-29 14:22:11 +0100

    images rendu accessibles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 //import cors from 'cors';
 const app = express();
 const PORT = 3008;
+app.use('/uploads', express.static(path.resolve('uploads')));
 
 // // Middleware pour gérer les CORS
 // app.use(cors({
